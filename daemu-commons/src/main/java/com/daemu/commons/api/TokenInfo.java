17119409f1d4eb3b8429298049a9fae6c3b20871
german_shepherd <719931804@qq.com>
2021-01-19 10:42:00 +0800

    去问问

---
+++ daemu-commons/src/main/java/com/daemu/commons/api/TokenInfo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.daemu.commons.api;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class TokenInfo {
+
+	private String access_token;
+
+	private String token_type;
+
+	private String refresh_token;
+	
+	private String scope;
+	
+	private Map<String,String> additionalInfo;
+ 	
+}
